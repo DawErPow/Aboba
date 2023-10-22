3b64c123d652ae907398449f4a2311cf53c0eba4
Pepexf <65292737+DawErPow@users.noreply.github.com>
2023-10-23 00:47:58 +0500

    Update CW.14.10.23.py

--- CW.14.10.23.py
+++ CW.14.10.23.py
@@ -25,15 +25,23 @@ def double_char(s):
     return "".join([c * 2 for c in s])
       
 #Шестое задание
-
+def smash(words):
+    return " ".join(words) if len(words) >= 1 else ""
       
 #Седьмое задание
+geese = {"African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"}
 
+def goose_filter(birds):
+    return [bird for bird in birds if bird not in geese]
       
 #Восьмое задание
-
+def count_positives_sum_negatives(arr):
+    return [sum(n > 0 for n in arr), sum(n for n in arr if n < 0)] if arr else []
       
 #Девятое задание
-
+def maps(a):
+    return [2 * x for x in a]
 
 #Десятое задание
+def find_needle(haystack):
+    return f'found the needle at position {haystack.index("needle")}'
